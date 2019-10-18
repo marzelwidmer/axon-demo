9f40a024d0a1a162f2919c297b2bfc9a7e31dcd0
Marcel Widmer <marzelwidmer@gmail.com>
2019-10-18 14:59:33 +0200

    Add other EventHandler

--- bank-account-query/src/main/kotlin/ch/keepcalm/account/api/BankAccountEventHandler.kt
+++ bank-account-query/src/main/kotlin/ch/keepcalm/account/api/BankAccountEventHandler.kt
@@ -10,6 +10,12 @@ class BankAccountEventHandler (private val bankAccountViewRepository: BankAccoun
     @EventHandler
     fun on (event: BankAccountCreatedEvent) = bankAccountViewRepository.save(BankAccountView(id = event.id, balance = event.balance))
 
+    @EventHandler
+    fun on (event: CashDepositedEvent) = bankAccountViewRepository.save(BankAccountView(id = event.id, balance = event.amount))
+
+    @EventHandler
+    fun on (event: CashWithdrawnEvent) = bankAccountViewRepository.save(BankAccountView(id = event.id, balance = event.amount))
+
     @QueryHandler
     fun answer(query: FindAllBankAccountsQuery) = bankAccountViewRepository.findAll()
 }
